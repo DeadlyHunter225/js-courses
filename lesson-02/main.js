035e197c8277515df621cf18696e748f040b92cd
D.Ovcharenko <mobileukraine5@gmail.com>
2018-06-28 19:23:24 +0300

    Task3

--- lesson-02/main.js
+++ lesson-02/main.js
@@ -7,11 +7,21 @@ for (let i = 1; i <= 5; i = i + 1) {
 document.write(result);
 */
 //
-let result2 = "";
+/*let result2 = "";
 
 for (let j = 5; j >= 1; j = j - 1) {
     result2 = result2 + j;
 }
 
 document.write(result2);
+*/
 //
+
+/*let a=3;
+
+for (let k = 1; k <= 10; k++) {
+    document.write (a + "*" + k + "=" + a * k + "<br>");
+}
+*/
+//
+
