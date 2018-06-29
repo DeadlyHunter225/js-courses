1bb1c0af299405e341ed8bed80a2f895cc3c52df
D.Ovcharenko <mobileukraine5@gmail.com>
2018-06-29 11:33:15 +0300

    Task4

--- lesson-02/main.js
+++ lesson-02/main.js
@@ -25,3 +25,17 @@ for (let k = 1; k <= 10; k++) {
 */
 //
 
+
+var x = Number(prompt("Введите число: "));
+var j = 0;
+if (x <= 0 ) {
+    alert("введите число большее нуля");
+    } else document.write(x);
+document.write("<br>");
+for (var i = 1; i <= x; i++) {
+document.write(i);
+if (i <= (x - 1)) document.write( " + " );
+j = j + i; 
+if (i == x) document.write( " = " + j);
+}
+document.write("<br>" + "Сумма чисел от 1 до "  + x + " равнa " + j);
